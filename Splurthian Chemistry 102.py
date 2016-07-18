3aa18100cbf0c6b57111cc8451892ebad133bf53
Malcolm Walters <MalWalters@users.noreply.github.com>
2016-07-18 12:01:25 +1000

    Update Splurthian Chemistry 102.py
    Now outputs correct answer

--- Splurthian Chemistry 102.py
+++ Splurthian Chemistry 102.py
@@ -19,29 +19,26 @@ def find_symbol(element):
             first_sym = element[:first_sym_pos]
         sec_sym = element[sec_sym_pos-1:sec_sym_pos]
         symbol = first_sym.upper() + sec_sym
-        #print(element + ": Trying: " + symbol)
         if not symbol in symbols:
             valid_symbol = True
         else:
-            if first_sym_pos == len(element):
-                print("No symbol for " + element)
-                return
             if sec_sym_pos == len(element):
                 first_sym_pos += 1
-                sec_sym_pos = first_sym_pos + 1
-                #print("** Out of combinations: " + element)
+                if first_sym_pos < len(element) - 1:
+                    sec_sym_pos = first_sym_pos + 1
+                else:
+                    if not element in no_match:
+                        no_match.append(element)
             else:
                 sec_sym_pos += 1          
-    #print(element)
-    #print("*1st: " + first_sym.upper() + " | 2nd: " + sec_sym)
-    #print("++ Adding " + symbol + " " + element + '\n')
-    if element == "Bartium":
-        print(element + " " + symbol)
-    symbols.append(symbol)
-
+    if len(symbol) == 2:
+        symbols.append(symbol)
 
 symbols = []
+no_match = []
 
 elements = open(element_list_file).read().split()
 for element in elements:
     find_symbol(element)
+
+print("The first one with no working elemental symbol is: " + no_match[0])
